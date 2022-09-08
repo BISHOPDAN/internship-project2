42cfc920d9b9a24eabf24f253659a81f57011eb0
BISHOPDAN <juliusdaniel554@gmail.com>
2022-09-09 00:59:10 +0100

    My guessing game

--- Program.cs
+++ Program.cs
@@ -2,20 +2,24 @@
 
 using System;
 
-public class LeapYear
+namespace Application
 {
-   public static void Main()
-   {
-      for (int year = 1995; year <= 2016; year++)
-      {
-         if (DateTime.IsLeapYear(year))
-         {
-            Console.WriteLine("{0} is a leap year.", year);
-            DateTime leapDay = new DateTime(year, 2, 29);
-            DateTime nextYear = leapDay.AddYears(1);
-            Console.WriteLine("   One year from {0} is {1}.", leapDay.ToString("d"), nextYear.ToString("d"));
-         }         
-      }
-   }
+
+    public class LeapYear
+    {
+        public static void Main()
+        {
+            for (int year = 1995; year <= 2016; year++)
+            {
+                if (DateTime.IsLeapYear(year))
+                {
+                Console.WriteLine("{0} is a leap year.", year);
+                DateTime leapDay = new DateTime(year, 2, 29);
+                DateTime nextYear = leapDay.AddYears(1);
+                Console.WriteLine("   One year from {0} is {1}.", leapDay.ToString("d"), nextYear.ToString("d"));
+                }         
+            }
+        }
+    }
 }
 
